b4bb8710b9389e4cbf1afccda75cc89a4ff92166
22020806-管弘毅 <purekoala@outlook.com>
2022-04-05 20:25:23 +0800

    now am in batch mode

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,14 +5,31 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
+/*  char *p = s;
+  uint64_t len = 0;
+  for(;*p;p++)
+  	len++;
+  return len;*/
   panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
+/*  uint64_t len = strlen(src);
+  for(uint64_t i=0; i<len; i++)
+  	dst[i] = src[i];
+  return dst;*/
   panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
+/*  uint64_t len = strlen(src);
+  for(uint64_t i = 0; i<n; i++){
+  	if(i<len)
+  		dst[i] = src[i];
+  	else
+  		dst[i] = '\0';
+  }
+  return dst;*/
   panic("Not implemented");
 }
 
